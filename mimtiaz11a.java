1459ff79f07106fe39431ad3a86128bd03ae8801
mustafaimtiazuddin <ctcuser97@ip-172-31-46-179.us-east-2.compute.internal>
2023-11-14 19:26:14 +0000

    java program

---
+++ mimtiaz11a.java
@@ -0,0 +1,21 @@
+//Mustafa Imtiazuddin
+//CSC 321
+//LAB 11
+
+import java.util.Scanner;
+
+public class mimtiaz11a {
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		int number, sqr;
+
+		System.out.println("please enter a number to square: ");
+		number = scan.nextInt();
+
+		sqr = number * number;
+
+		System.out.println("the answer is " +sqr+ " fly like a butterfly, sting like a bee");
+	}
+}
